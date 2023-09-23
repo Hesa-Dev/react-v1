64e515bc623d67802b3efada927a7ee5cb8e0fee
hesa-dev <geral@hesasoft.com>
2023-09-24 01:43:01 +0200

    update files

--- src/components/hooks/ImageAleatoria.tsx
+++ src/components/hooks/ImageAleatoria.tsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
+import React, { useState } from 'react';
+
 export default function ImagemAleatoria() {
 
-    const url='https://source.unsplash.com/featured/300x300?'
-    let pesquisa:string ="";
+    const [pesquisa, alterarPesquisa] = useState<string>('abstract')
+    const [tamanho, alterarTamanho] = useState<number>(300)
+    const url = 'https://source.unsplash.com/featured/300x300?'
+    // let pesquisa: string = "";
 
-    function renderizarBotao(valor:string) {
+    function urlImagem() {
+
+        return `${url}
+               ${pesquisa} `
+    }
+
+    function renderizarBotao(valor: string) {
 
         return (
 
@@ -12,24 +23,28 @@ export default function ImagemAleatoria() {
              justify-center items-center
              text-white
              px-4 py-2 rounded-md
-            `} onClick={()=> {
-                
-                pesquisa = valor
-                console.log(pesquisa)
-             } }>
-               {valor} 
+            `} onClick={() => {
+
+                    alterarPesquisa (valor)
+                    console.log(urlImagem())
+                }}>
+                {valor}
             </button>
         )
 
     }
     return (
 
-        <div className={`
-       flex flex-col gap-3  `}>
+        <div className={`flex flex-col gap-3 border border-zinc-500 p-7 rounded-md`}>
+       
+       <Image src={urlImagem()}  height={300} width={300} alt="Imagem"/>
 
-            {renderizarBotao('abastract')}
-            {renderizarBotao('city')}
-            {renderizarBotao('person')}
+            <div className={` flex  gap-5 justify-between `}>
+                {renderizarBotao('abastract')}
+                {renderizarBotao('city')}
+                {renderizarBotao('person')}
+            </div>
+
         </div>
     )
 }
