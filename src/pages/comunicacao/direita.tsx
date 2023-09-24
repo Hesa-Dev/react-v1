3fa63e3dcdafb2e18ab009c6aba06357754c98de
hesa-dev <geral@hesasoft.com>
2023-09-24 22:43:51 +0200

    comunicacao direita entre componentesv2

--- src/pages/comunicacao/direita.tsx
+++ src/pages/comunicacao/direita.tsx
@@ -4,9 +4,10 @@ export default function PaginaDireita(){
 
  return (
 
-    <div className="flex justify-center flex-col items-center h-screen">
+    <div className="flex justify-center flex-col items-center h-screen gap-5">
         <h1 className="text-5xl mb-10 bg-slate-400"> comunicação direita</h1>
         <Avo nome="Jose" sobrenome="Pedro" />
+        <Avo nome="Vasco" sobrenome="Ribeiro" />
     </div>
  )
 
