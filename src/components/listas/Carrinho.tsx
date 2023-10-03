02ac44452865045b96ab89340c068b87ac1b1694
hesa-dev <geral@hesasoft.com>
2023-10-03 22:28:28 +0200

    listas de produtos

--- src/components/listas/Carrinho.tsx
+++ src/components/listas/Carrinho.tsx
@@ -1,12 +1,29 @@
 import ItemCarrinho from "@/model/ItemCarrinho"
+import CarrinhoItem from "./CarrinhoItem"
 
-interface CarrinhoProps{
+interface CarrinhoProps {
 
-    itens:ItemCarrinho[]
+    itens: ItemCarrinho[]
 }
 
 
-export default function Carrinho(props:CarrinhoProps){
+export default function Carrinho(props: CarrinhoProps) {
 
-    return
+    return (
+
+        <div className="flex flex-col w-full ">
+
+            <div className="flex bg-zinc-500 text-3xl justify-center p-3 text-white font-bold items-center ">
+                Carrinho 
+            </div>
+            <div className="p-5">
+                {props.itens.map((item, i) => {
+
+                    return (
+                        <CarrinhoItem key={i} {...item} />
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
--- src/pages/listas/produtos.tsx
+++ src/pages/listas/produtos.tsx
@@ -1,3 +1,4 @@
+import Carrinho from "@/components/listas/Carrinho"
 import ListaProdutos from "@/components/listas/ListaProdutos"
 // import ProdutoItem from "@/components/listas/ProdutoItem"
 import { produtos } from "@/constants/produtos"
@@ -15,17 +16,26 @@ export default function PaginaProdutos() {
     function adicionarProduto(produto: Produto){
 
 
-        console.log(produto)
+        // console.log(produto) 
+        // ... -> pega a ultima váriavel da linha de cima ex:. 
 
+        // const a =3 
+        // const b =4 
+        // const soma = (...b + a )
+
+        const itemAtual = itens.find((itens)=>itens.produto.id==produto.id) ?? {quantidade:0,produto}
+        const novoItem = {...itemAtual , quantidade : itemAtual.quantidade +1}
+        const outrosItens = itens.filter((itens) => itens.produto)
+        setItens([...outrosItens,novoItem])
     }
     return (
 
         <div className={`
             flex justify-center items-center h-screen
-            flex-col
+            flex-col gap-10
         `}>
             {/* <ProdutoItem produto={produto} />  */}
-
+<Carrinho  itens={itens} />
             <ListaProdutos produtos={produtos} comprar={adicionarProduto} />
         </div>
 
