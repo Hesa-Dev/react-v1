9bc687ee4e0936fd87dd1e45c6fe97c00c4bf1e8
hesa-dev <geral@hesasoft.com>
2023-10-08 14:30:11 +0200

    update files useContext Context API

--- src/data/contexts/CarrinhoContexto.tsx
+++ src/data/contexts/CarrinhoContexto.tsx
@@ -16,6 +16,14 @@ export function CarrinhoProvider(props: any) {
 
     const [itens, setItens] = useState<ItemCarrinho[]>([])
     function adicionarProduto(produto: Produto) {
+
+        // console.log(produto) 
+        // ... -> pega a ultima váriavel da linha de cima ex:. 
+
+        // const a =3 
+        // const b =4 
+        // const soma = (...b + a )
+        
         const itemAtual = itens.find((itens) => itens.produto.id == produto.id) ?? { quantidade: 0, produto }
         const novoItem = { ...itemAtual, quantidade: itemAtual.quantidade + 1 }
         const outrosItens = itens.filter((itens) => itens.produto)
